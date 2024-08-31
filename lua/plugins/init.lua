f7d1787e0bc0151ad25d850eb8a3907f03d2d9a7
ziggoon <zackmusgrave20@gmail.com>
2024-08-31 10:47:44 -0500

    add avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,9 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -22,4 +20,45 @@ return {
   		},
   	},
   },
+
+  {
+"yetone/avante.nvim",
+event = "VeryLazy",
+lazy = false,
+opts = {
+  providers="claude",
+},
+keys = {
+  { "<leader>aa", function() require("avante.api").ask() end, desc = "avante: ask", mode = { "n", "v" } },
+  { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
+  { "<leader>ae", function() require("avante.api").edit() end, desc = "avante: edit", mode = "v" },
+},
+dependencies = {
+  "stevearc/dressing.nvim",
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+},
+}
 }
